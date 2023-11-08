e7bbb8245eae8ebc63733c113ce068f3efc2de6c
Nina Høegh-Larsen <ninius@online.no>
2023-11-08 21:27:14 +0100

    Fixed function name to actually say what it does

--- src/03.py
+++ src/03.py
@@ -2,7 +2,7 @@
 
 from tools import files
 
-def is_valid(a, b, c):
+def is_invalid(a, b, c):
     return (a + b <= c) or (a + c <= b) or (b + c <= a)
 
 filename = "input/03.txt"
@@ -15,7 +15,7 @@ counter = 0
 for line in input:
     a, b, c = [int(i) for i in line.split()]
 
-    if is_valid(a, b, c):
+    if is_invalid(a, b, c):
         counter += 1
 
 print("Part 1 The number of valid triangles are {}".format(len(input) - counter))
@@ -29,13 +29,13 @@ for x in range(0, len(input), 3):
     b, e, h = [int(j) for j in input[x+1].split()]
     c, f, i = [int(j) for j in input[x+2].split()]
 
-    if is_valid(a, b, c):
+    if is_invalid(a, b, c):
         counter += 1
 
-    if is_valid(d, e, f):
+    if is_invalid(d, e, f):
         counter += 1
 
-    if is_valid(g, h, i):
+    if is_invalid(g, h, i):
         counter += 1
 
 print("Part 2: The number of valid triangles are {}".format(len(input) - counter))
