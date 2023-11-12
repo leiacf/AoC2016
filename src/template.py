2ca50b4586fa25e316aa8969383d3c9a06c6b2ff
Nina Høegh-Larsen <ninius@online.no>
2023-11-12 14:45:00 +0100

    Small template for not wasting time

---
+++ src/template.py
@@ -0,0 +1,19 @@
+#Advent of Code 2016 Day X
+
+from tools import files
+
+
+
+filename = "input/.py"
+input = files.input_as_list(filename)
+
+
+# Part 1
+
+
+print("Part 1: {}".format())
+
+# Part 2
+
+
+print("Part 2: {}".format())
