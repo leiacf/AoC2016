bd82a00bc78353c17ce03f412ee8983f463eff3a
Nina Høegh-Larsen <ninius@online.no>
2023-11-19 23:25:35 +0100

    Fixed the template a bit

--- src/template.py
+++ src/template.py
@@ -1,19 +1,14 @@
-#Advent of Code 2016 Day X
+#Advent of Code 2016 Day XX
 
 from tools import files
 
-
-
-filename = "input/.txt"
+filename = "input/XX.txt"
 input = files.input_as_list(filename)
 
-
 # Part 1
-
 
 print("Part 1: {}".format(1))
 
 # Part 2
-
 
 print("Part 2: {}".format(2))
